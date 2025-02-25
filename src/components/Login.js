40d7657ce126799631df15e87800517781b0418d
Asliddin <asliddinilhomov3@gmail.com>
2025-02-25 17:13:49 +0500

    fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,56 +1,86 @@
-import React from 'react';
-import styles from '../styles/Sign.module.css';
-import Link from 'next/link';
+import React from "react";
+import styles from "../styles/Sign.module.css";
+import Link from "next/link";
 
 function Login() {
-    return (
-        <div className={styles['signUp']}>
-            <div className={styles['sign']}>
-                <div className={styles['getStart']}>
-                    <h1 className={styles['welcome']}>Welcome back!</h1>
-                    <p className={styles['enter']}>Enter your Credentials to access your account</p>
-                    <div className={styles['part-2']}>
-                        <label className={styles['lab']} htmlFor="email">Email address</label>
-                        <input className={styles['place']} placeholder='Enter your email' type="text" id="email" />
-                    </div>
-                    <div className={styles['part-3']}>
-                        <div className={styles['lab']}>
-                            <label className={styles['lab']} htmlFor="password">Password</label>
-                            <Link href="#">Forgot password</Link>
-                        </div>
-                        <input className={styles['place']} placeholder='Enter your password' type="password" id="password" />
-                    </div>
-                    <div className={styles['part-4']}>
-                        <input type="checkbox" />
-                        <p>Remember for 30 days</p>
-                    </div>
-                    <button className={styles['btnSign']}>Login</button>
-
-                    <div className={styles['part-5']}>
-                        <div className={styles['hr-1']}><hr /></div>
-                        <div className={styles['or']}><p>or</p></div>
-                        <div className={styles['hr-1']}><hr /></div>
-                    </div>
-                    <div className={styles['part-6']}>
-                        <button className={styles['btn']}>
-                            <img style={{ width: '24px', height: '24px' }} src="/w.png" alt="Google" />
-                            <p>Sign in with Google</p>
-                        </button>
-                        <button className={styles['btn']}>
-                            <img style={{ width: '24px', height: '24px' }} src="/apple.png" alt="Apple" />
-                            <p>Sign in with Apple</p>
-                        </button>
-                    </div>
-                    <div className={styles['part-7']}>
-                        <p>Don't have an account? <Link href="/login">Sign Up</Link></p>
-                    </div>
-                </div>
+  return (
+    <div className={styles["signUp"]}>
+      <div className={styles["sign"]}>
+        <div className={styles["getStart"]}>
+          <h1 className={styles["welcome"]}>Welcome back!</h1>
+          <p className={styles["enter"]}>
+            Enter your Credentials to access your account
+          </p>
+          <div className={styles["part-2"]}>
+            <label className={styles["lab"]} htmlFor="email">
+              Email address
+            </label>
+            <input
+              className={styles["place"]}
+              placeholder="Enter your email"
+              type="text"
+              id="email"
+            />
+          </div>
+          <div className={styles["part-3"]}>
+            <div className={styles["lab"]}>
+              <label className={styles["lab"]} htmlFor="password">
+                Password
+              </label>
+              <Link href="#">Forgot password</Link>
             </div>
-            <div className={styles['sign-picture']}>
-                <img src="/barg.png" alt="Background" />
+            <input
+              className={styles["place"]}
+              placeholder="Enter your password"
+              type="password"
+              id="password"
+            />
+          </div>
+          <div className={styles["part-4"]}>
+            <input type="checkbox" />
+            <p>Remember for 30 days</p>
+          </div>
+          <button className={styles["btnSign"]}>Login</button>
+          <div className={styles["part-5"]}>
+            <div className={styles["hr-1"]}>
+              <hr />
             </div>
+            <div className={styles["or"]}>
+              <p>or</p>
+            </div>
+            <div className={styles["hr-1"]}>
+              <hr />
+            </div>
+          </div>
+          <div className={styles["part-6"]}>
+            <button className={styles["btn"]}>
+              <img
+                style={{ width: "24px", height: "24px" }}
+                src="/w.png"
+                alt="Google"
+              />
+              <p>Sign in with Google</p>
+            </button>
+            <button className={styles["btn"]}>
+              <img
+                style={{ width: "24px", height: "24px" }}
+                src="/apple.png"
+                alt="Apple"
+              />
+              <p>Sign in with Apple</p>
+            </button>
+          </div>
+          <div className={styles["part-7"]}>
+            <p>Dont have an account? <Link href="/login">Sign Up</Link>
+            </p>
+          </div>
         </div>
-    );
+      </div>
+      <div className={styles["sign-picture"]}>
+        <img src="/barg.png" alt="Background" />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/components/Sign.js
+++ src/components/Sign.js
@@ -1,55 +1,94 @@
-import React from 'react';
-import styles from '../styles/Sign.module.css';
+import React from "react";
+import styles from "../styles/Sign.module.css";
+import Link from "next/link";
 
 function Sign() {
-    return (
-        <div className={styles['signUp']}>
-            <div className={styles['sign']}>
-                <div className={styles['getStart']}>
-                    <h1 className={styles['get']}>Get Started Now</h1>
-                    <div className={styles['part-1']}>
-                        <label className={styles['lab']} htmlFor="name">Name</label>
-                        <input className={styles['place']} id="name" placeholder='Enter your name' type="text" />
-                    </div>
-                    <div className={styles['part-2']}>
-                        <label className={styles['lab']} htmlFor="email">Email address</label>
-                        <input className={styles['place']} id="email" placeholder='Enter your email' type="text" />
-                    </div>
-                    <div className={styles['part-3']}>
-                        <label className={styles['lab']} htmlFor="password">Password</label>
-                        <input className={styles['place']} id="password" placeholder='Enter your password' type="password" />
-                    </div>
-                    <div className={styles['part-4']}>
-                        <input type="checkbox" />
-                        <p>I agree to the <a className={styles['term']} href="#">terms & policy</a></p>
-                    </div>
-                    <button className={styles['btnSign']}>Log in</button>
+  return (
+    <div className={styles["signUp"]}>
+      <div className={styles["sign"]}>
+        <div className={styles["getStart"]}>
+          <h1 className={styles["get"]}>Get Started Now</h1>
+          <div className={styles["part-1"]}>
+            <label className={styles["lab"]} htmlFor="name">
+              Name
+            </label>
+            <input
+              className={styles["place"]}
+              id="name"
+              placeholder="Enter your name"
+              type="text"
+            />
+          </div>
+          <div className={styles["part-2"]}>
+            <label className={styles["lab"]} htmlFor="email">
+              Email address
+            </label>
+            <input
+              className={styles["place"]}
+              id="email"
+              placeholder="Enter your email"
+              type="text"
+            />
+          </div>
+          <div className={styles["part-3"]}>
+            <label className={styles["lab"]} htmlFor="password">
+              Password
+            </label>
+            <input
+              className={styles["place"]}
+              id="password"
+              placeholder="Enter your password"
+              type="password"
+            />
+          </div>
+          <div className={styles["part-4"]}>
+            <input type="checkbox" />
+            <p>
+              I agree to the{" "}
+              <a className={styles["term"]} href="#">
+                terms &amp; policy
+              </a>
+            </p>
+          </div>
+          <button className={styles["btnSign"]}>Log in</button>
 
-                    <div className={styles['part-5']}>
-                        <div className={styles['hr-1']}><hr /></div>
-                        <div className={styles['or']}><p>or</p></div>
-                        <div className={styles['hr-1']}><hr /></div>
-                    </div>
-                    <div className={styles['part-6']}>
-                        <button className={styles['btn']}>
-                            <img style={{ width: '24px', height: '24px' }} src="/w.png" alt="Google" />
-                            <p>Sign in with Google</p>
-                        </button>
-                        <button className={styles['btn']}>
-                            <img style={{ width: '24px', height: '24px' }} src="/apple.png" alt="Apple" />
-                            <p>Sign in with Apple</p>
-                        </button>
-                    </div>
-                    <div className={styles['part-7']}>
-                        <p>Have an account? <Link href="/register">Sign In</Link></p>
-                    </div>
-                </div>
+          <div className={styles["part-5"]}>
+            <div className={styles["hr-1"]}>
+              <hr />
             </div>
-            <div className={styles['sign-picture']}>
-                <img src="/barg.png" alt="Background" />
+            <div className={styles["or"]}>
+              <p>or</p>
             </div>
+            <div className={styles["hr-1"]}>
+              <hr />
+            </div>
+          </div>
+          <div className={styles["part-6"]}>
+            <button className={styles["btn"]}>
+              <img
+                style={{ width: "24px", height: "24px" }}
+                src="/w.png"
+                alt="Google"
+              />
+              <p>Sign in with Google</p>
+            </button>
+            <button className={styles["btn"]}>
+              <img style={{ width: "24px", height: "24px" }} src="/apple.png" alt="Apple"/>
+              <p>Sign in with Apple</p>
+            </button>
+          </div>
+          <div className={styles["part-7"]}>
+            <p>
+              Have an account? <Link href="/register">Sign In</Link>
+            </p>
+          </div>
         </div>
-    );
+      </div>
+      <div className={styles["sign-picture"]}>
+        <img src="/barg.png" alt="Background" />
+      </div>
+    </div>
+  );
 }
 
 export default Sign;
